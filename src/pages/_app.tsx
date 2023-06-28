f3139b63b6b66f25103791c920b72915d54ddcc7
Dhika Rizky <deka_r@outlook.com>
2023-06-28 23:32:17 +0700

    Fix Next migration issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,12 +22,7 @@ export default function App ({ Component, pageProps }: AppProps): React.ReactEle
       <ThemeProvider theme={Theme}>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-          <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;700&display=swap" rel="stylesheet" />
           <title>Shiori &amp; Friends, oleh Shiori Labs</title>
-          <link rel="shortcut icon" href="https://shiorilabs.id/favicon.png" type="image/png" />
         </Head>
         <Global />
         <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -32,7 +32,13 @@ export default class MyDocument extends Document {
   render (): React.ReactElement {
     return (
       <Html lang="id">
-        <Head />
+        <Head>
+          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+          <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;700&display=swap" rel="stylesheet" />
+          <link rel="shortcut icon" href="https://shiorilabs.id/favicon.png" type="image/png" />
+        </Head>
         <body>
           <Main />
           <NextScript />
