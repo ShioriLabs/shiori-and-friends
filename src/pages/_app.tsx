d7b24c9fb7656815fce69b9cd4d856b2cf1deeab
Dhika Rizky <deka_r@outlook.com>
2023-06-28 23:01:32 +0700

    Fix typing in crossOrigin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,7 +24,7 @@ export default function App ({ Component, pageProps }: AppProps): React.ReactEle
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;700&display=swap" rel="stylesheet" />
           <title>Shiori &amp; Friends, oleh Shiori Labs</title>
           <link rel="shortcut icon" href="https://shiorilabs.id/favicon.png" type="image/png" />
